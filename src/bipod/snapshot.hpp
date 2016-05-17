883a445b7e324abbc28d03bd57db777d9686291b
shigT <11024306@mmm.muroran-it.ac.jp>
2016-05-17 14:09:31 +0900

    Add muiti-field operability to snapshot.Get<T>()

--- src/bipod/snapshot.hpp
+++ src/bipod/snapshot.hpp
@@ -35,15 +35,50 @@ public:
 		std::get<Field>(outbox_).value = optional<typename Field::type>(val);
 	}
 	
+	template<class Field>
+	optional<typename Field::type> Get(typename std::enable_if<detail::is_in<Field, std::tuple<ReadPermissions...>>::value>::type* = nullptr) {
+		return std::get<Field>(inbox_).value;
+	}
+
+
+	template<class... Fields>
+	optional<std::tuple<Fields...>> Get(typename std::enable_if<(sizeof...(Fields) > 1) && detail::are_in<std::tuple<Fields...>, std::tuple<ReadPermissions...>>::value>::type* = nullptr) {
+		if (are_true(std::get<Fields>(inbox_).value...)) {
+			return boost::make_optional(std::make_tuple(std::get<Fields>(inbox_)...));
+		}
+		return boost::none;
+	}
+
+	/*
 	template<class Field, class = typename std::enable_if<detail::is_in<Field, std::tuple<ReadPermissions...>>::value>::type>
 	optional<typename Field::type> Get() {
 		return std::get<Field>(inbox_).value;
 	}
+
+	
+	template<class... Fields, class = typename std::enable_if<(sizeof...(Fields) > 1) && detail::are_in<std::tuple<Fields...>, std::tuple<ReadPermissions...>>::value>::type>
+	optional<std::tuple<Fields...>> Get() {
+		return boost::none;
+	}
+	*/
+	
 	
 private:
 	ConnectionType& conn_;
 	const inbox_t inbox_;
 	outbox_t outbox_;
+
+	template<class Head>
+	bool are_true(Head head) {
+		return static_cast<bool>(head);
+	}
+
+	template<class Head, class... Tail>
+	bool are_true(Head head, Tail... tail) {
+		return static_cast<bool>(head) && are_true(tail...);
+	}
+
+
 };
 
 } // end namespace bipod
